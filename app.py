92d04c9db3b3b7fe802d21792fe199711b4a2c5e
Md Mazid Hossain <mazid.info67@gmail.com>
2025-08-22 00:29:43 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, DevOps! This is a hands-on project."
+    return "Hei, DevOps! This is a hands-on project."
 
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=5000)
